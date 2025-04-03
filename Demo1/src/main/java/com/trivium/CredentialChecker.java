26f983be74857f4a0d869b250659cc615ac8f9c0
Guru <gurumurthy.ramamurthy@gmail.com>
2025-04-03 15:37:16 +0530

    I added a dummy method

--- Demo1/src/main/java/com/trivium/CredentialChecker.java
+++ Demo1/src/main/java/com/trivium/CredentialChecker.java
@@ -5,4 +5,13 @@ public class CredentialChecker {
 		// Dummy implementation for credential checking
 		return "admin".equals(username) && "password".equals(password);
 	}
-}
+	
+	public static void dummyMethod() {
+		// This method is intentionally left blank
+        // It serves as a placeholder for future functionality
+        // or to demonstrate the structure of the class
+        // without any specific implementation at this time.
+        System.out.println("This is a dummy method.");
+    }
+	}
+
